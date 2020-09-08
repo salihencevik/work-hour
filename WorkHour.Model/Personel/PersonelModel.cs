3520718e9e73363c3a1bcfc43fa0f7d90602c189
Salih Encevik <salihencevik@gmail.com>
2020-09-08 10:03:46 +0300

    düzeltme

--- WorkHour.Model/Personel/PersonelModel.cs
+++ WorkHour.Model/Personel/PersonelModel.cs
@@ -14,5 +14,6 @@ namespace WorkHour.Model
         public string Adress { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
+        public List<int?> Roles { get; set; }
     }
 }
