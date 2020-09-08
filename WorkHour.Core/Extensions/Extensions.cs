4515e433ce709fa080902d1248dd4993db7293e6
Muhammed Kalyon <goksel9a@gmail.com>
2020-09-08 09:37:27 +0300

    extensions getpropertyvalues overload edildi

--- WorkHour.Core/Extensions/Extensions.cs
+++ WorkHour.Core/Extensions/Extensions.cs
@@ -36,6 +36,31 @@ namespace WorkHour.Core
 
             return obj;
         }
+        public static T GetPropertyValues<T>(this object selfmodel,T Targetmodel) where T : class
+        {
+      
+            Type targetmodeltype = Targetmodel.GetType();
+            Type selfmodeltype = selfmodel.GetType();
+
+            PropertyInfo[] targetprops = targetmodeltype.GetProperties();
+            PropertyInfo[] selfprops = selfmodeltype.GetProperties();
+
+            foreach (var targetitem in targetprops)
+            {
+                foreach (var selfitem in selfprops)
+                {
+                    if (selfitem.Name == targetitem.Name)
+                    {
+                        var deneme2 = selfitem.GetValue(selfmodel, null);
+
+                        targetitem.SetValue(Targetmodel, deneme2);
+                    }
+
+                }
+            }
+
+            return Targetmodel;
+        }
 
     }
 }
