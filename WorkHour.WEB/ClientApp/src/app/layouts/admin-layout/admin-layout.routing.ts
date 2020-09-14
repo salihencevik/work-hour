a6c7454bb2140392c7d615461365157e13b07296
Salih Encevik <salihencevik@gmail.com>
2020-09-14 09:48:44 +0300

    Routing düzenlendi

--- WorkHour.WEB/ClientApp/src/app/layouts/admin-layout/admin-layout.routing.ts
+++ WorkHour.WEB/ClientApp/src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { PersonelComponent } from '../../pages/personel/personel.component';
+import { ShiftComponent } from '../../pages/shift/shift.component';
  
 
 export const AdminLayoutRoutes: Routes = [
-  { path: 'personel', component: PersonelComponent } 
+  { path: 'Personel', component: PersonelComponent },
+  { path: 'Shift', component: ShiftComponent },
 ];
